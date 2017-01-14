86974c2effef595c5f0103f968776d1f9370af25
Siggy Hinds <Siggy@Siggys-iMac.home>
2017-01-13 23:26:07 -0800

    changed message to be an array, added event to speak button and clear, still working on back button

--- client/src/components/SpeechBar.js
+++ client/src/components/SpeechBar.js
@@ -5,21 +5,78 @@ class SpeechBar extends Component {
     constructor(props) {
         super(props);
 
+        this.messageString = this.messageString.bind(this);
+        this.speakMessage = this.speakMessage.bind(this);
+        this.handleBackSpace = this.handleBackSpace.bind(this);
+        this.handleClear = this.handleClear.bind(this);
+
         this.state = {
-            message: "message placeholder...",
-                    //this is the message appearing in the message window
-                    //TODO change to diff data structure (array)
+            message: ['I', 'Love', 'you'], // array: message appearing in the message window
         }
+    }
+
+
+
+    //reduces the message array into one string (improves interpretation of speech)
+    messageString() {
+        if(this.state.message.length > 1)
+            return this.state.message.join(' ');
+    }
+
+
+
+    speakMessage(e) {
+        e.preventDefault();
+
+        var msg = new SpeechSynthesisUtterance();
+
+        if (this.state.message !== '') { msg.text = this.messageString(); }
+        else { msg.text= "The message window is empty." }
+
+        window.speechSynthesis.speak(msg);
+    }
+
+
+
+    handleBackSpace(e) {
+        e.preventDefault();
+
+        // var updateMsg = this.state.message.pop();
+        // this.setState({ message: updateMsg });
+
+
+        // if(this.state.message.length >= 0) {
+        //     debugger
+        //     var newMessage = this.state.message.pop()
+        //     this.setState({message: newMessage});
+        //     console.log('after backspace ' + this.state.message);
+        // }
+
+        this.setState(state => {
+            this.state.message.pop();
+            return {items: this.state.message};
+        })
 
     }
 
+
+
+    handleClear() {
+        this.setState({message: ""});
+    }
+
+
+
     render() {
+
+        var text = this.messageString()
+
         return (
-            <div>
-                <button>Play</button>
-                <div id="messageWindow">{this.state.message} </div>
-                <button>Back</button>
-                <button>Clear</button>
+            <div id="speechBar" style={{ margin: "auto", border: "solid", color: "green" }}>
+                <button onClick={this.speakMessage} > Play </button>
+                <div id="messageWindow">{ text } </div>
+                <button > BackSpace</button>
+                <button onClick={this.handleClear} >Clear</button>
             </div>
 
         );

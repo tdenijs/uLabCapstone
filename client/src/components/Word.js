530a0e927293141d59c4fcb2c8ab3186f2257d07
Simone <simonetalla@yahoo.fr>
2017-01-16 14:22:44 -0800

    make word speak

--- client/src/components/Word.js
+++ client/src/components/Word.js
@@ -5,18 +5,30 @@ class Word extends Component {
     constructor(props) {
         super(props)
 
+        this.speak = this.speak.bind(this);
+
         this.state = {
             text: "Love",
             symbol: "Symbol"
         }
     }
 
+    speak (){
+      var word = new SpeechSynthesisUtterance(this.state.text);
+      window.speechSynthesis.speak(word);
+    }
+
+    clicked (){
+        alert("this is just a test");
+    }
     render() {
        return (
            <div>
-               {this.state.symbol}
+               <div>
+                  {this.state.symbol}
+               </div>
                 <br/>
-                {this.state.text}
+                <button onClick={this.speak}>{this.state.text}</button>
             </div>
        );
     }

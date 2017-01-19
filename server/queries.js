d8868266bd228d8de0f792b56fee84bfd11da0aa
nosracq <carson_14@outlook.com>
2017-01-19 00:00:20 -0800

    define getAllWords

--- server/queries.js
+++ server/queries.js
@@ -5,13 +5,29 @@ const options = {
 };
 
 const pgp = require('pg-promise')(options);
-const connectionString = 'postgres://localhost:5432/ulabdb';
+const username = "postgres";
+const password = "portland";
+const connectionString = 'postgres://' + username + ':' + password + '@localhost:5432/ulabdb';
 const db = pgp(connectionString);
 
 //============================================
 // Query functions
 //============================================
 
+function getAllWords(req, res, next) {
+  db.any('select * from Words')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved all words'
+        })
+    })
+    .catch(function (err) {
+      return next(err);
+    })
+}
 module.exports = {
   getAllWords: getAllWords
 }

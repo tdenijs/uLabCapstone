a81d1c34435932333b79e79d20328536d5252cd4
nosracq <carson_14@outlook.com>
2017-01-19 00:01:14 -0800

    define route /api/words

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const router = express.Router();
+const db = require('./queries')
 const app = express();
 const fs = require('fs');
 
 // Node middleware used to handle JSON, raw, text and url encoded form data
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 // Create a urlencoded parser for post requests
-const urlencodedParser = bodyParser.urlencoded({extended: false });
+//const urlencodedParser = bodyParser.urlencoded({extended: false });
 
 // Node middleware used to parser cookie header and populate req.cookie-parser
 // with an object keyed by the cookie names
 const cookieParser = require('cookie-parser');
 
 // Node middleware used to handle multipart/form data
-const multer = require('multer');
+//const multer = require('multer');
 
 app.use(cookieParser());
 
@@ -23,10 +25,19 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+//===============================================
+// Routes
+//===============================================
+
+router.get('/api/words', db.getAllWords);
+
+/*
 app.get('/', function(req, res) {
   res.send('Welcome to the API Server!')
 });
-
+*/
 app.listen(app.get('port'), function() {
   console.log('Find the API server at: http://localhost:3001/');
 });
+
+module.exprts = router;

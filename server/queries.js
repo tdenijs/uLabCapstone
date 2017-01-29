1b87ab6e7202cdbf7c2c93de51fb29e1402fad9a
nosracq <carson_14@outlook.com>
2017-01-28 16:07:31 -0800

    implemented getAllWordsByListName

--- server/queries.js
+++ server/queries.js
@@ -21,16 +21,36 @@ function getAllWords(req, res, next) {
   db.any('select * from Words')
     .then(function (data) {
       res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved all words'
-        })
+        .json(data)
     })
     .catch(function (err) {
       return next(err);
     })
 }
+function getAllWordsByListName(req, res, next) {
+  var lTitle = req.params.title;
+  console.log('\''+req.params.title+'\'');
+  db.any("select l.list_id, l.list_title, lw.word_id, w.word, w.symbol_id, s.symbol_path, s.symbol_text "
+         + "from lists l inner join listwords lw on l.list_id=lw.list_id "
+         + "inner join words w on lw.word_id=w.word_id "
+         + "inner join symbols s on w.symbol_id=s.symbol_id "
+         + "where l.list_title=" + '\'' + lTitle + '\'')
+    .then(function (data) {
+      if (data.length > 0) {
+        res.status(200)
+          .json(data);
+          console.log("Data was sent");
+      } else {
+        res.status(404)
+        .send("Not Found");
+        console.log("ERROR (404)");
+      }
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
 module.exports = {
-  getAllWords: getAllWords
+  getAllWords: getAllWords,
+  getAllWordsByListName: getAllWordsByListName
 }

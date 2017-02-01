ffed6971f8194da77c221a50e99a165fcf84c73d
Anton Zipper <anton.zipper@gmail.com>
2017-02-01 01:54:45 -0800

    Sorted columns in grid so they always display in the right order

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -4,10 +4,13 @@ import _ from 'lodash';
 
 class Grid extends Component {
     render() {
+        // Make sure the cols are in the right order
+        let sortedCols = _.sortBy(this.props.cols, 'title');
+
         return(
             <div id="grid">
                 {
-                    this.props.cols.map(({title, words}) => {
+                    sortedCols.map(({title, words}) => {
                         return (<Column key={_.uniqueId()} title={title} words={words} add={this.props.add}/>);
                     })
                 }

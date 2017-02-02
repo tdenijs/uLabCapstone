5120a00ef8bb55d96e02a05874b1ec1995f1fdf7
bluesun <cmonk@pdx.edu>
2017-02-02 14:43:57 -0800

    fixed merge conflicts, syntax errors, and changed api call to getAllWordsByGridName

--- server/queries.js
+++ server/queries.js
@@ -63,27 +63,29 @@ function getAllWordsByListName(req, res, next) {
 function getAllWordsByListId(req, res, next) {
   var id = req.params.id;
   db.any("select l.list_id, l.list_title, lw.word_id, w.word, w.symbol_id, s.symbol_path, s.symbol_text "
-         + "from lists l inner join listwords lw on l.list_id=lw.list_id "
-         + "inner join words w on lw.word_id=w.word_id "
-         + "inner join symbols s on w.symbol_id=s.symbol_id "
-         + "where l.list_id=" + '\'' + id + '\'')
-  .then(function (data) {
-  if (data.length > 0) {
-  res.status(200)
-  .json(data);
-  console.log("All words for list id " + id + " were sent.");
-} else {
-  res.status(404)
-  .send("ERROR: List id " + '\'' + id + '\' ' + "not found");
-    console.log("ERROR (404)");
-  }
-  })
+    + "from lists l inner join listwords lw on l.list_id=lw.list_id "
+    + "inner join words w on lw.word_id=w.word_id "
+    + "inner join symbols s on w.symbol_id=s.symbol_id "
+    + "where l.list_id=" + '\'' + id + '\'')
+    .then(function (data) {
+      if (data.length > 0) {
+        res.status(200)
+          .json(data);
+        console.log("All words for list id " + id + " were sent.");
+      } else {
+        res.status(404)
+          .send("ERROR: List id " + '\'' + id + '\' ' + "not found");
+        console.log("ERROR (404)");
+      }
+    })
     .catch(function (err) {
       return next(err);
     });
+}
+
 
 // Returns all lists, words, symbols for grid by grid name
-function getAllGridWordsByGridName (req, res, next) {
+function getAllWordsByGridName (req, res, next) {
   var gTitle = req.params.title;
   db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_name, s.symbol_path, s.symbol_text "
        + "from grids g inner join gridlists gl on g.grid_id=gl.grid_id "
@@ -113,6 +115,6 @@ module.exports = {
     getAllWords: getAllWords,
     getAllWordsByListName: getAllWordsByListName,
     getAllWordsByListId: getAllWordsByListId,
-    getAllGridWordsByGridName: getAllWordsByGridName
+    getAllWordsByGridName: getAllWordsByGridName
 }
 

6ad0bace29727f808df47e6a3e04348633e5e2f6
bluesun <cmonk@pdx.edu>
2017-02-01 19:16:39 -0800

    added api function getAllGridWordsByGridName in query.js

--- server/queries.js
+++ server/queries.js
@@ -35,6 +35,7 @@ function getAllWords(req, res, next) {
       return next(err);
     });
 }
+
 function getAllWordsByListName(req, res, next) {
   var lTitle = req.params.title;
   db.any("select l.list_id, l.list_title, lw.word_id, w.word, w.symbol_id, s.symbol_path, s.symbol_text "
@@ -57,6 +58,7 @@ function getAllWordsByListName(req, res, next) {
       return next(err);
     });
 }
+
 function getAllWordsByListId(req, res, next) {
   var id = req.params.id;
   db.any("select l.list_id, l.list_title, lw.word_id, w.word, w.symbol_id, s.symbol_path, s.symbol_text "
@@ -64,14 +66,40 @@ function getAllWordsByListId(req, res, next) {
          + "inner join words w on lw.word_id=w.word_id "
          + "inner join symbols s on w.symbol_id=s.symbol_id "
          + "where l.list_id=" + '\'' + id + '\'')
+  .then(function (data) {
+  if (data.length > 0) {
+  res.status(200)
+  .json(data);
+  console.log("All words for list id " + id + " were sent.");
+} else {
+  res.status(404)
+  .send("ERROR: List id " + '\'' + id + '\' ' + "not found");
+    console.log("ERROR (404)");
+  }
+  })
+    .catch(function (err) {
+      return next(err);
+    });
+
+
+// Returns all lists, words, symbols for grid by grid name
+function getAllGridWordsByGridName (req, res, next) {
+  var gTitle = req.params.name;
+  db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_name, s.symbol_path, s.symbol_text"
+      + "from grids g inner join gridlists gl on g.grid_id=gl.grid_id"
+      + "inner join lists l on l.list_id=gl.list_id"
+      + "inner join listwords lw on l.list_id=lw.list_id"
+      + "inner join words w on w.word_id=lw.word_id"
+      + "inner join symbols s on w.symbol_id=s.symbol_id"
+      + "where g.grid_title=" + '\'' + gTitle + '\'' +  "order by word_id")
     .then(function (data) {
       if (data.length > 0) {
         res.status(200)
           .json(data);
-          console.log("All words for list id " + id + " were sent.");
+          console.log("All words for grid " + gTitle + " were sent.");
       } else {
         res.status(404)
-        .send("ERROR: List id " + '\'' + id + '\' ' + "not found");
+        .send("ERROR: Grid " + '\'' + gTitle + '\' ' + "not found");
         console.log("ERROR (404)");
       }
     })
@@ -79,8 +107,11 @@ function getAllWordsByListId(req, res, next) {
       return next(err);
     });
 }
+
 module.exports = {
-  getAllWords: getAllWords,
-  getAllWordsByListName: getAllWordsByListName,
-  getAllWordsByListId: getAllWordsByListId
+    getAllWords: getAllWords,
+    getAllWordsByListName: getAllWordsByListName,
+    getAllWordsByListId: getAllWordsByListId,
+    getAllGridWordsByGridName: getAllWordsByGridName
 }
+

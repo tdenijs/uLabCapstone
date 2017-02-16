81b8ca19014264bac965b5eb79a12202a759fea8
bluesun <cmonk@pdx.edu>
2017-02-15 21:44:34 -0800

    fixed syntax error in getAllListWordsByListId query, add api tests for this call

--- server/queries.js
+++ server/queries.js
@@ -187,20 +187,20 @@ function getAllListsByGridID(req, res, next) {
 
 
 function getAllListWordsByListId(req, res, next) {
-  var gridId = req.params.title;
-  var listId = req.params.title;
-  db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_path "
+  var gridId = req.params.grid_id;
+  var listId = req.params.list_id;
+  db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_path, s.symbol_text "
          + "from grids g inner join gridlists gl on g.grid_id=gl.grid_id "
          + "inner join lists l on l.list_id = gl.list_id "
          + "inner join listwords lw on l.list_id = lw.list_id "
          + "inner join words w on w.word_id = lw.word_id "
          + "inner join symbols s on w.symbol_id = s.symbol_id "
-         + "where g.grid_id = " + '\'' + gridId + '\'' + "and where l.list_id = " + '\'' + listId + '\'')
+         + "where g.grid_id= " + '\'' + gridId + '\'' + " and l.list_id = " + '\'' + listId + '\'' )
     .then(function (data) {
       if (data.length > 0) {
         res.status(200)
           .json(data);
-          console.log("All words for list " + listId + " were sent.");
+          console.log("All words for list " + '\'' + listId + '\'' + " were sent.");
       } else {
         res.status(404)
         .send("ERROR: List " + '\'' + listId + '\' ' + "not found");

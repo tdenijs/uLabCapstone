adf3daefbee898a7c815f8a596127b25d939a44c
siggy-h <siegby@yahoo.com>
2017-02-17 18:13:47 -0800

    added WordEditor property so test would function

--- client/__tests__/WordEditor.test.js
+++ client/__tests__/WordEditor.test.js
@@ -5,6 +5,8 @@ import WordEditor from '../src/components/WordEditor.js';
 
 it('WordEditor renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<WordEditor />, div);
+    ReactDOM.render(<WordEditor coreListTitles={["hello", "friend"]} />, div);
+
+//    coreListTitles
 });
 

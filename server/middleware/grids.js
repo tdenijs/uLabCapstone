194f589a1a33e4119a8d7a13ee7ca102820bee20
jiaqiluo <jiaqiluo123+GitHub@gmail.com>
2017-02-24 00:26:58 -0800

    1. improve the delete api. 2. unifily ident of BE codes.

--- server/middleware/grids.js
+++ server/middleware/grids.js
@@ -9,20 +9,19 @@
 const db = require('../config/dbconnect');
 
 function getAllGrids(req, res, next) {
-      db.any('SELECT g.grid_id, g.grid_title FROM grids g')
-    .then(function (data) {
+  db.any('SELECT g.grid_id, g.grid_title FROM grids g')
+    .then(function(data) {
       if (data.length > 0) {
-      res.status(200)
-        .json(data)
+        res.status(200)
+          .json(data)
         console.log("All grids were sent.")
-      }
-      else {
+      } else {
         res.status(404)
           .send("ERROR: No grid found");
-          console.log("ERROR (404)");
+        console.log("ERROR (404)");
       }
     })
-    .catch(function (err) {
+    .catch(function(err) {
       return next(err);
     });
 }
@@ -30,14 +29,14 @@ function getAllGrids(req, res, next) {
 // Returns all lists, words, symbols for grid by grid id
 function getAllWordsByGridId(req, res, next) {
   var gridId = req.params.grid_id;
-  db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_name, s.symbol_path, s.symbol_text "
-    + "from grids g inner join gridlists gl on g.grid_id=gl.grid_id "
-    + "inner join lists l on l.list_id=gl.list_id "
-    + "inner join listwords lw on l.list_id=lw.list_id "
-    + "inner join words w on w.word_id=lw.word_id "
-    + "inner join symbols s on w.symbol_id=s.symbol_id "
-    + "where g.grid_id="+ '\'' + gridId + '\'' + " order by w.word_id")
-    .then(function (data) {
+  db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_name, s.symbol_path, s.symbol_text " +
+      "from grids g inner join gridlists gl on g.grid_id=gl.grid_id " +
+      "inner join lists l on l.list_id=gl.list_id " +
+      "inner join listwords lw on l.list_id=lw.list_id " +
+      "inner join words w on w.word_id=lw.word_id " +
+      "inner join symbols s on w.symbol_id=s.symbol_id " +
+      "where g.grid_id=" + '\'' + gridId + '\'' + " order by w.word_id")
+    .then(function(data) {
       if (data.length > 0) {
         res.status(200)
           .json(data);
@@ -48,7 +47,7 @@ function getAllWordsByGridId(req, res, next) {
         console.log("ERROR (404)");
       }
     })
-    .catch(function (err) {
+    .catch(function(err) {
       return next(err);
     });
 }
@@ -56,14 +55,14 @@ function getAllWordsByGridId(req, res, next) {
 // Returns all lists, words, symbols for grid by grid name
 function getAllWordsByGridName(req, res, next) {
   var gTitle = req.params.grid_title;
-  db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_name, s.symbol_path, s.symbol_text "
-    + "from grids g inner join gridlists gl on g.grid_id=gl.grid_id "
-    + "inner join lists l on l.list_id=gl.list_id "
-    + "inner join listwords lw on l.list_id=lw.list_id "
-    + "inner join words w on w.word_id=lw.word_id "
-    + "inner join symbols s on w.symbol_id=s.symbol_id "
-    + "where g.grid_title="+ '\'' + gTitle + '\'' + " order by w.word_id")
-    .then(function (data) {
+  db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_name, s.symbol_path, s.symbol_text " +
+      "from grids g inner join gridlists gl on g.grid_id=gl.grid_id " +
+      "inner join lists l on l.list_id=gl.list_id " +
+      "inner join listwords lw on l.list_id=lw.list_id " +
+      "inner join words w on w.word_id=lw.word_id " +
+      "inner join symbols s on w.symbol_id=s.symbol_id " +
+      "where g.grid_title=" + '\'' + gTitle + '\'' + " order by w.word_id")
+    .then(function(data) {
       if (data.length > 0) {
         res.status(200)
           .json(data);
@@ -74,7 +73,7 @@ function getAllWordsByGridName(req, res, next) {
         console.log("ERROR (404)");
       }
     })
-    .catch(function (err) {
+    .catch(function(err) {
       return next(err);
     });
 }
@@ -84,23 +83,23 @@ function getAllWordsByGridName(req, res, next) {
 //      [{"grid_id": , "list_id": , "list_title": }, ...]
 function getAllListsByGridID(req, res, next) {
   var targetGridID = req.params.grid_id;
-  db.any('SELECT gl.grid_id, gl.list_id, l.list_title '
-         + 'FROM gridlists gl '
-         + 'INNER JOIN lists l ON gl.list_id=l.list_id '
-         + 'WHERE gl.grid_id=' + '\'' + targetGridID + '\' '
-         + 'ORDER BY gl.list_id asc')
-    .then(function (data) {
+  db.any('SELECT gl.grid_id, gl.list_id, l.list_title ' +
+      'FROM gridlists gl ' +
+      'INNER JOIN lists l ON gl.list_id=l.list_id ' +
+      'WHERE gl.grid_id=' + '\'' + targetGridID + '\' ' +
+      'ORDER BY gl.list_id asc')
+    .then(function(data) {
       if (data.length > 0) {
         res.status(200)
           .json(data);
-          console.log("All lists for grid_id " + targetGridID + " were sent.");
+        console.log("All lists for grid_id " + targetGridID + " were sent.");
       } else {
         res.status(404)
-        .send("ERROR: grid_id " + '\'' + targetGridID + '\' ' + "not found");
+          .send("ERROR: grid_id " + '\'' + targetGridID + '\' ' + "not found");
         console.log("ERROR (404)");
       }
     })
-    .catch(function (err) {
+    .catch(function(err) {
       return next(err);
     });
 }
@@ -108,33 +107,33 @@ function getAllListsByGridID(req, res, next) {
 function getAllListWordsByListId(req, res, next) {
   var gridId = req.params.grid_id;
   var listId = req.params.list_id;
-  db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_path, s.symbol_text "
-         + "from grids g inner join gridlists gl on g.grid_id=gl.grid_id "
-         + "inner join lists l on l.list_id = gl.list_id "
-         + "inner join listwords lw on l.list_id = lw.list_id "
-         + "inner join words w on w.word_id = lw.word_id "
-         + "inner join symbols s on w.symbol_id = s.symbol_id "
-         + "where g.grid_id= " + '\'' + gridId + '\'' + " and l.list_id = " + '\'' + listId + '\'' )
-    .then(function (data) {
+  db.any("select g.grid_id, g.grid_title, l.list_id, l.list_title, w.word_id, w.word, s.symbol_id, s.symbol_path, s.symbol_text " +
+      "from grids g inner join gridlists gl on g.grid_id=gl.grid_id " +
+      "inner join lists l on l.list_id = gl.list_id " +
+      "inner join listwords lw on l.list_id = lw.list_id " +
+      "inner join words w on w.word_id = lw.word_id " +
+      "inner join symbols s on w.symbol_id = s.symbol_id " +
+      "where g.grid_id= " + '\'' + gridId + '\'' + " and l.list_id = " + '\'' + listId + '\'')
+    .then(function(data) {
       if (data.length > 0) {
         res.status(200)
           .json(data);
-          console.log("All words for list " + '\'' + listId + '\'' + " were sent.");
+        console.log("All words for list " + '\'' + listId + '\'' + " were sent.");
       } else {
         res.status(404)
-        .send("ERROR: List " + '\'' + listId + '\' ' + "not found");
+          .send("ERROR: List " + '\'' + listId + '\' ' + "not found");
         console.log("ERROR (404)");
       }
     })
-    .catch(function (err) {
+    .catch(function(err) {
       return next(err);
     });
 }
 
 module.exports = {
-    getAllGrids: getAllGrids,
-    getAllWordsByGridId: getAllWordsByGridId,
-    getAllWordsByGridName: getAllWordsByGridName,
-    getAllListWordsByListId: getAllListWordsByListId,
-    getAllListsByGridID: getAllListsByGridID
+  getAllGrids: getAllGrids,
+  getAllWordsByGridId: getAllWordsByGridId,
+  getAllWordsByGridName: getAllWordsByGridName,
+  getAllListWordsByListId: getAllListWordsByListId,
+  getAllListsByGridID: getAllListsByGridID
 }
--- server/middleware/lists.js
+++ server/middleware/lists.js
@@ -10,12 +10,12 @@ const db = require('../config/dbconnect');
 
 function getAllWordsByListId(req, res, next) {
   var id = req.params.id;
-  db.any("select l.list_id, l.list_title, lw.word_id, w.word, w.symbol_id, s.symbol_path, s.symbol_text "
-    + "from lists l inner join listwords lw on l.list_id=lw.list_id "
-    + "inner join words w on lw.word_id=w.word_id "
-    + "inner join symbols s on w.symbol_id=s.symbol_id "
-    + "where l.list_id=" + '\'' + id + '\'')
-    .then(function (data) {
+  db.any("select l.list_id, l.list_title, lw.word_id, w.word, w.symbol_id, s.symbol_path, s.symbol_text " +
+      "from lists l inner join listwords lw on l.list_id=lw.list_id " +
+      "inner join words w on lw.word_id=w.word_id " +
+      "inner join symbols s on w.symbol_id=s.symbol_id " +
+      "where l.list_id=" + '\'' + id + '\'')
+    .then(function(data) {
       if (data.length > 0) {
         res.status(200)
           .json(data);
@@ -26,58 +26,62 @@ function getAllWordsByListId(req, res, next) {
         console.log("ERROR (404)");
       }
     })
-    .catch(function (err) {
+    .catch(function(err) {
       return next(err);
     });
 }
 
 function getAllWordsByListName(req, res, next) {
   var lTitle = req.params.title;
-  db.any("select l.list_id, l.list_title, lw.word_id, w.word, w.symbol_id, s.symbol_path, s.symbol_text "
-         + "from lists l inner join listwords lw on l.list_id=lw.list_id "
-         + "inner join words w on lw.word_id=w.word_id "
-         + "inner join symbols s on w.symbol_id=s.symbol_id "
-         + "where l.list_title=" + '\'' + lTitle + '\'')
-    .then(function (data) {
+  db.any("select l.list_id, l.list_title, lw.word_id, w.word, w.symbol_id, s.symbol_path, s.symbol_text " +
+      "from lists l inner join listwords lw on l.list_id=lw.list_id " +
+      "inner join words w on lw.word_id=w.word_id " +
+      "inner join symbols s on w.symbol_id=s.symbol_id " +
+      "where l.list_title=" + '\'' + lTitle + '\'')
+    .then(function(data) {
       if (data.length > 0) {
         res.status(200)
           .json(data);
-          console.log("All words for list " + lTitle + " were sent.");
+        console.log("All words for list " + lTitle + " were sent.");
       } else {
         res.status(404)
-        .send("ERROR: List " + '\'' + lTitle + '\' ' + "not found");
+          .send("ERROR: List " + '\'' + lTitle + '\' ' + "not found");
         console.log("ERROR (404)");
       }
     })
-    .catch(function (err) {
+    .catch(function(err) {
       return next(err);
     });
 }
 
 function deleteWordByID(req, res, next) {
-  console.log("deleteWordByID starts");
   var targetListID = parseInt(req.params.list_id);
   var targetWordID = parseInt(req.params.word_id);
-  db.result('delete from listwords where list_id=$1 AND word_id=$2',[targetListID, targetWordID])
-  .then(function (result) {
-     res.status(200)
-       .json({
-         status: 'success',
-         message: `Removed ${result.rowCount} word`
-       });
-       console.log("Removed " + result.rowCount + " word");
-      //  res.status(404)
-      //  .send("ERROR: word_id: " + targetWordID + " not found in list_id: " + targetListID);
-      //  console.log("ERROR (404)");
-     })
-   .catch(function (err) {
-     return next(err);
-   });
+  var query = 'select * from listwords where list_id=' + targetListID + ' and word_id=' + targetWordID;
+  console.log(query);
+  db.any(query)
+    .then(function(data) {
+      if (data.length > 0) {
+        db.result('delete from listwords where list_id=$1 AND word_id=$2', [targetListID, targetWordID])
+          .then(function(result) {
+            res.status(200)
+              .json({
+                status: 'success',
+                message: `Removed ${result.rowCount} word`
+              });
+          })
+      } else {
+        res.status(404)
+          .send("The word does not exist in the list.");
+      }
+    })
+    .catch(function(err) {
+      return next(err);
+    });
 }
 
-
 module.exports = {
-    getAllWordsByListId: getAllWordsByListId,
-    getAllWordsByListName: getAllWordsByListName,
-    deleteWordByID: deleteWordByID
+  getAllWordsByListId: getAllWordsByListId,
+  getAllWordsByListName: getAllWordsByListName,
+  deleteWordByID: deleteWordByID
 }

9279f186ef1777c0df0a1e1201b9cf26f29cd108
sh <siegby@yahoo.com>
2017-03-03 14:37:19 -0800

    more tweaking

--- client/src/components/Column.js
+++ client/src/components/Column.js
@@ -13,20 +13,25 @@ import React, {Component} from 'react';
 import Word from './Word'
 import _ from 'lodash';
 
+// style={{height: this.props.maxHeight}}
+
 class Column extends Component {
   render() {
     return (
       <div className="Column" >
         <div className="ColumnTitle">{this.props.title}</div>
-        {
-          this.props.words.map(({word_id, word, symbol_path, alt}) => {
-            return (
-              <Word key={_.uniqueId()} id={word_id} text={word} src={symbol_path} alt={alt} add={this.props.add}
-                    selectedVoice={this.props.selectedVoice} editorToggle={this.props.editorToggle}
-                    col_id={this.props.col_id} removeFromGrid={this.props.removeFromGrid} />
-            );
-          })
-        }
+        <div className="WordContainer">
+          {
+            this.props.words.map(({word_id, word, symbol_path, alt}) => {
+              return (
+                <Word key={_.uniqueId()} id={word_id} text={word} src={symbol_path}
+                      alt={alt} add={this.props.add} selectedVoice={this.props.selectedVoice}
+                      editorToggle={this.props.editorToggle}
+                      col_id={this.props.col_id} removeFromGrid={this.props.removeFromGrid}/>
+              );
+            })
+          }
+        </div>
       </div>
     );
   }
@@ -38,7 +43,7 @@ Column.propTypes = {
 };
 
 Column.defaultProps = {
-  words: [{id: "1", word:"love", symbol_path:"", alt:""}]
+  words: [{id: "1", word: "love", symbol_path: "", alt: ""}]
 };
 
 export default Column;
